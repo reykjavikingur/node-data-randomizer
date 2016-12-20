33bdde180cb6ac8192500025907819554c4ebb48
Andrew Strader <astrader@resourceammirati.com>
2016-12-20 13:19:16 -0500

    add methods to create random integers, booleans, seeds

--- test/randomizer.spec.js
+++ test/randomizer.spec.js
@@ -135,6 +135,161 @@ describe('Randomizer', ()=> {
 
 		});
 
+		describe('.integers', ()=> {
+
+			describe('from 0 to 1', ()=> {
+
+				var randomInteger;
+
+				beforeEach(()=> {
+					randomInteger = random.integers(0, 1);
+				});
+
+				it('should be a function', ()=> {
+					should(randomInteger).be.a.Function();
+				});
+
+				describe('values', ()=> {
+					var values;
+					beforeEach(()=> {
+						values = {};
+						for (let i = 0; i < 1000; i++) {
+							let x = randomInteger();
+							if (!values.hasOwnProperty(x)) {
+								values[x] = 0;
+							}
+							values[x]++;
+						}
+					});
+					it('should sometimes return 0', ()=> {
+						should(values[0]).be.ok();
+					});
+					it('should sometimes return 1', ()=> {
+						should(values[1]).be.ok();
+					});
+				});
+
+			});
+
+			describe('25 to 50', ()=> {
+				var min, max, randomInteger;
+				beforeEach(()=> {
+					min = 25;
+					max = 50;
+					randomInteger = random.integers(min, max);
+				});
+				it('should always return value not less than min', ()=> {
+					for (var i = 0; i < 1000; i++) {
+						should(randomInteger()).not.be.lessThan(min);
+					}
+				});
+				it('should always return value not greater than max', ()=> {
+					for (let i = 0; i < 1000; i++) {
+						should(randomInteger()).not.be.greaterThan(max);
+					}
+				});
+				it('should always return value rounded to nearest 1', ()=> {
+					for (let i = 0; i < 1000; i++) {
+						var x = randomInteger();
+						var y = Math.round(x);
+						should(x).equal(y);
+					}
+				});
+			});
+
+		});
+
+		describe('.booleans', ()=> {
+
+			describe('with default split', ()=> {
+				var randomBoolean;
+				beforeEach(()=> {
+					randomBoolean = random.booleans();
+				});
+				describe('values', ()=> {
+					var values;
+					beforeEach(()=> {
+						values = {
+							'true': 0,
+							'false': 0,
+							'other': 0
+						};
+						for (let i = 0; i < 1000; i++) {
+							let x = randomBoolean();
+							if (x === true) {
+								values['true']++;
+							}
+							else if (x === false) {
+								values['false']++;
+							}
+							else {
+								values['other']++;
+							}
+						}
+					});
+					it('should sometimes return true', ()=> {
+						should(values['true']).be.greaterThan(0);
+					});
+					it('should sometimes return false', ()=> {
+						should(values['false']).be.greaterThan(0);
+					});
+					it('should never return value other than true or false', ()=> {
+						should(values['other']).equal(0);
+					});
+				});
+			});
+
+			describe('with full bias towards false', ()=> {
+
+				var randomBoolean;
+				beforeEach(()=> {
+					randomBoolean = random.booleans(0);
+				});
+				it('should always return false', ()=> {
+					for (let i = 0; i < 1000; i++) {
+						should(randomBoolean()).eql(false);
+					}
+				});
+			});
+
+			describe('with full bias towards true', ()=> {
+				var randomBoolean;
+				beforeEach(()=> {
+					randomBoolean = random.booleans(1);
+				});
+				it('should always return false', ()=> {
+					for (let i = 0; i < 1000; i++) {
+						should(randomBoolean()).eql(true);
+					}
+				});
+			});
+
+		});
+
+		describe('.seeds', ()=> {
+
+			var randomSeed;
+			beforeEach(()=> {
+				randomSeed = random.seeds();
+			});
+
+			it('should always return a string', ()=> {
+				for (let i = 0; i < 1000; i++) {
+					should(randomSeed()).be.a.String();
+				}
+			});
+
+			it('should generate distinct string every time', ()=> {
+				let values = {}, n = 1000;
+				for (let i = 0; i < n; i++) {
+					var seed = randomSeed();
+					values[seed] = true;
+				}
+				let distinctSeeds = Object.keys(values);
+				should(distinctSeeds.length).equal(n);
+			});
+		});
+
 	});
 
 });
